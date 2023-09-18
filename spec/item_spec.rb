d67dbb4cc70414d84b2777d4a81ff7a0a62a53d5
Eric Belongea <eric.belongea@gmail.com>
2023-09-18 09:08:01 -0600

    Setup and item functionality

---
+++ spec/item_spec.rb
@@ -0,0 +1,16 @@
+require './lib/item'
+
+RSpec.describe '#Item' do 
+  before(:each) do
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+  describe '#Existance' do
+    it 'has exists with attributes' do
+      expect(@item1).to be_instance_of Item
+
+      expect(@item1.name).to eq('Peach')
+      expect(@item1.price).to eq('$0.75')
+    end
+  end
+end
